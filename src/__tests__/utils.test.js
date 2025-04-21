c00217a1c2c2d5993b898989e58e7df48bdd1db9
Brian <musyimib449@gmail.com>
2025-04-22 00:44:05 +0300

    tdd writing unit test  lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,40 @@
 // Your tests here
+// Your code here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns words combination of upper and lowercase ", () => {
+        const word = "raCecAr"
+        const result = isPalindrome(word)
+
+        expect(result).toBe(true)
+    })
+    it("backward or frontward combination ", () => {
+        const word = "racecar"
+        const  result = isPalindrome(word)
+
+        expect(result).toBe(true)
+    })
+    it(" return false for a non-palindrome", () => {
+        const word = "car"
+        const result = isPalindrome(word)
+
+        expect(result).toBe(false)
+    })
+    it("return false for an empty string ", () => {
+        const word = " "
+
+        expect(() => isPalindrome(word)).toThrow("Invalid input")
+    })
+
+    it("throw error fro non-alpabetical characters ", () => {
+        const word = " race car"
+
+        expect(() => isPalindrome(word)).toThrow("Invalid input")
+    })
+
+    it("throw error if input is not a string ", () => {
+        const word = 12345;
+        expect(() => isPalindrome(word)).toThrow("Invalid input");
+    })
+})
